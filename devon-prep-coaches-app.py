b05f46a43c33288bd2abad332de4732659876ec0
Dan Bannon <danbannon9@gmail.com>
2024-09-12 22:41:48 -0400

    added calculated columns

--- devon-prep-coaches-app.py
+++ devon-prep-coaches-app.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import streamlit as st
 from sqlalchemy import create_engine, text
+from datetime import date
+import math
+from decimal import Decimal
 
 db = create_engine("postgresql://postgres.xtmfmfkpgommfdujhvev:G#v2!*hq8hU8-aU@aws-0-us-east-1.pooler.supabase.com:6543/postgres").connect()
+
+classdict = {
+        0: "grad",
+        1: "freshman",
+        2: "sophomore",
+        3: "junior",
+        4: "senior",
+        5: "middle"
+}
 
 def authenticate():
     st.sidebar.header('Login')
@@ -17,27 +29,33 @@ def authenticate():
 if authenticate():
     players = pd.DataFrame(db.execute(text("SELECT * FROM players")))
     players.set_index('id',inplace=True)
+    players = pd.DataFrame(db.execute(text("SELECT * FROM players")))
+    players.set_index('id',inplace=True)
+    def classdef(thing):
+        class_years = []
+        for thing in players['grad_year']:
+            if isinstance(thing, Decimal):
+                thing = int(thing)
+            years_diff = math.ceil((date(thing, 9, 1) - date.today()).days / 365)
+            if years_diff >= 5:
+                return 5
+            elif years_diff <1:
+                return 0
+            class_year = classdict.get(years_diff)
+            class_years.append(class_year)
+        players['class'] = class_years
+    classdef(players['grad_year'])
 
-###### PICK UP HERE, ADD IN CLASS AND OTHER FORMULA COLUMNS ######
-    
     coaches = pd.DataFrame(db.execute(text("SELECT * FROM coaches")))
     coaches.set_index('id',inplace=True)
 
     notes = pd.DataFrame(db.execute(text("SELECT * FROM notes")))
     notes.set_index('id',inplace=True)
 
-    #clean column names
-    def clean_column_names(columns):
-        return ['is_pitcher' if col == 'Pitcher?' else col.lower().replace(' ', '_') for col in columns]
-
-    players.columns = clean_column_names(players.columns)
-
     players['full_name'] = players['first_name'] + ' ' + players['last_name']
 
     active_classes = ['Freshman','Sophomore','Junior','Senior']
     players['active'] = players['class'].isin(active_classes)
-
-    pnotes = pd.read_csv("https://raw.githubusercontent.com/dbannon9/devon_prep_baseball/master/pnotes.csv")
 
     #streamlit app
 
@@ -45,25 +63,24 @@ if authenticate():
     st.subheader('Track Player Updates Below')
     ptoggle = st.toggle('Pitchers?')
     if ptoggle:
-        fplayers = players.query('is_pitcher == True')[['first_name','last_name','class','level']]
+        fplayers = players.query('pitcher == True')[['first_name','last_name','class','level']]
     else:
-        fplayers = players[['first_name','last_name','class','level','position_1','position_2','position_3']].fillna('')
+        fplayers = players[['first_name','last_name','class','pos_1','pos_2','pos_3']].fillna('')
     fplayers
 
-    #pitcher notes
+    #player notes
 
-    st.title('Input New Pitcher Note')
-    currentpitchers = players.query('is_pitcher == True').query('active == True')
-    authors = ['Coach Palumbo','Coach Grande','Coach Bannon','Coach Martin','Coach Toal','Coach Kania']
+    st.title('Input New Player Note')
+    currentplayers = players.query('active == True')
 
-    with st.form(key='Input New Pitcher Note'):
-        pnote_pitcher = st.selectbox("Pitcher", currentpitchers['full_name'])
-        pnote_date = st.date_input("Today's Date", value = "default_value_today")
-        pnote_author = st.selectbox("Author", authors)
-        pnote_note = st.text_input("Note")
-        pnote_submit = st.form_submit_button(label = "Submit Note")
+    with st.form(key='Input New Player Note'):
+        note_pitcher = st.selectbox("Player", currentplayers['full_name'])
+        note_date = st.date_input("Today's Date", value = "default_value_today")
+        note_coach = st.selectbox("Coach", coaches['name'])
+        note_note = st.text_input("Note")
+        note_submit = st.form_submit_button(label = "Submit Note")
 
-    if pnote_submit:
-        new_note = {'pitcher': pnote_pitcher, 'date': pnote_date, 'author': pnote_author, 'note': pnote_note}
+    if note_submit:
+        new_note = {'pitcher': note_pitcher, 'date': note_date, 'coach': note_coach, 'note': note_note}
         pd.DataFrame([new_note])
-        pnotes = pd.concat([pnotes, new_note], ignore_index=True)
+        pnotes = pd.concat([notes, new_note], ignore_index=True)
