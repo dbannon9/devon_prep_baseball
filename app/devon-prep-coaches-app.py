f18355df0f2ab6fa536f3ef2c132b099ab40fca2
Dan Bannon <danbannon9@gmail.com>
2024-09-27 10:51:16 -0400

    wide mode

--- app/devon-prep-coaches-app.py
+++ app/devon-prep-coaches-app.py
@@ -8,13 +8,18 @@ from decimal import Decimal
 import os
 from supabase import create_client, Client
 
-#%% Authentication
+#%% Make Page Wide
+st.set_page_config(layout="wide")
+
+#%% page definitions
 
 roster = st.Page("pages/roster-page.py",title="Roster",icon=":material/group:")
 note_input = st.Page("pages/note-input-page.py",title="Input Notes",icon=":material/edit_note:")
 player_page = st.Page("pages/player-page.py",title="Player Summary",icon=":material/bar_chart:")
 # coach_page = st.Page("pages/coaches-page.py",title="Coach Summary",icon=":material/sports:")
 calendar_page = st.Page("pages/calendar-page.py",title="Schedule",icon=":material/calendar_month:")
+
+#%% Authentication
 
 def authenticate():
     st.sidebar.header('Login')
