742be9b6255598c64b407df8b52a8fd874a8fc9c
Dan Bannon <danbannon9@gmail.com>
2024-09-27 15:45:08 -0400

    Added team notes

--- app/pages/note-input-page.py
+++ app/pages/note-input-page.py
@@ -37,6 +37,8 @@ coaches = fetch_table_data('coaches')
 coaches.set_index('id',inplace=True)
 notes = fetch_table_data('notes')
 notes.set_index('id',inplace=True)
+team_notes = fetch_table_data('team_notes')
+team_notes.set_index('id',inplace=True)
 
 #%% Data Adjustments
 
@@ -83,37 +85,71 @@ currentplayers = players.query('active == True')
 player_options = players['full_name'].to_dict()
 coach_options = coaches['name'].to_dict()
 
-#%% Player Notes
+#%% Notes
 
-st.title('Input New Player Note')
+st.title('Input New Note')
 
-with st.form(key='Input New Player Note',clear_on_submit=True):
-    note_pitcher = st.selectbox("Player", options=list(player_options.keys()), format_func=lambda id: player_options[id])  # Displays name
-    note_date = st.date_input("Today's Date", value=date.today())  # Default value to today's date
-    note_coach = st.selectbox("Coach", options=list(coach_options.keys()), format_func=lambda id: coach_options[id])  # Displays name
-    note_type = st.selectbox("Type", note_types)  # Dropdown for note types
-    note_note = st.text_input("Note")  # Input field for the note text
-    note_submit = st.form_submit_button(label="Submit Note")
+note_level = st.selectbox('Player Note or Team Note', ["Player","Team"])
 
-# When form is submitted
-if note_submit:
-    # Convert the date object to ISO 8601 string format
-    note_date_str = note_date.isoformat()  # Converts the date object to 'YYYY-MM-DD'
+if note_level == "Player":
 
-    # Create new note dictionary with IDs
-    new_note = {
-        'player_id': note_pitcher,
-        'date': note_date_str,  # Use the string version of the date
-        'coach_id': note_coach,
-        'type': note_type,
-        'note': note_note
-    }
+    with st.form(key='Input New Player Note',clear_on_submit=True):
+        note_pitcher = st.selectbox("Player", options=list(player_options.keys()), format_func=lambda id: player_options[id])  # Displays name
+        note_date = st.date_input("Today's Date", value=date.today())  # Default value to today's date
+        note_coach = st.selectbox("Coach", options=list(coach_options.keys()), format_func=lambda id: coach_options[id])  # Displays name
+        note_type = st.selectbox("Type", note_types)  # Dropdown for note types
+        note_note = st.text_input("Note")  # Input field for the note text
+        note_submit = st.form_submit_button(label="Submit Note")
 
-    # Push using insert function
-    response = supabase.table("notes").insert(new_note).execute()
+    # When form is submitted
+    if note_submit:
+        # Convert the date object to ISO 8601 string format
+        note_date_str = note_date.isoformat()  # Converts the date object to 'YYYY-MM-DD'
 
-    # Mark the form as submitted
-    st.session_state.form_submitted = True
+        # Create new note dictionary with IDs
+        new_note = {
+            'player_id': note_pitcher,
+            'date': note_date_str,  # Use the string version of the date
+            'coach_id': note_coach,
+            'type': note_type,
+            'note': note_note
+        }
 
-    # Display success message
-    st.success("Note submitted successfully")
+        # Push using insert function
+        response = supabase.table("notes").insert(new_note).execute()
+
+        # Mark the form as submitted
+        st.session_state.form_submitted = True
+
+        # Display success message
+        st.success("Note submitted successfully")
+
+elif note_level == "Team":
+        with st.form(key='Input New Team Note',clear_on_submit=True):
+            team_note_date = st.date_input("Today's Date", value=date.today())  # Default value to today's date
+            team_note_coach = st.selectbox("Coach", options=list(coach_options.keys()), format_func=lambda id: coach_options[id])  # Displays name
+            team_note_note = st.text_input("Note")  # Input field for the note text
+            team_note_submit = st.form_submit_button(label="Submit Note")
+        # When form is submitted
+        if team_note_submit:
+            # Convert the date object to ISO 8601 string format
+            team_note_date_str = team_note_date.isoformat()  # Converts the date object to 'YYYY-MM-DD'
+
+            # Create new note dictionary with IDs
+            new_team_note = {
+                'date': team_note_date_str,  # Use the string version of the date
+                'coach_id': team_note_coach,
+                'note': team_note_note
+            }
+
+            # Push using insert function
+            response = supabase.table("team_notes").insert(new_team_note).execute()
+
+            # Mark the form as submitted
+            st.session_state.form_submitted = True
+
+            # Display success message
+            st.success("Note submitted successfully")
+
+elif note_level is None:
+    print("Please pick a note type: Player or Team")
