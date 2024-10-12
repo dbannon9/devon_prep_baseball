97beef192082d22d29a8526c71b74f650feb523d
Dan Bannon <danbannon9@gmail.com>
2024-10-12 14:09:43 -0400

    adding event 2

--- app/pages/view-practice-plans-page.py
+++ app/pages/view-practice-plans-page.py
@@ -49,7 +49,7 @@ else:
     # event_1_name = this_practice.iloc[0]['event_1_name']
     st.subheader(f"{str(this_practice.iloc[0]['event_1_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_1_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_1_name']}")
     st.write(f"{this_practice.iloc[0]['event_1_notes']}")
-    if this_practice.iloc[0]['event_2_name'].empty:
+    if this_practice.iloc[0]['event_2_name'] == "":
         print("")
     else:
         st.subheader(f"{str(this_practice.iloc[0]['event_2_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_2_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_2_name']}")
