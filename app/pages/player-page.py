3fe83600f7e251f3aed2de895b12e3044cb78317
Dan Bannon <danbannon9@gmail.com>
2024-11-01 16:37:45 -0400

    testing video

--- app/pages/player-page.py
+++ app/pages/player-page.py
@@ -113,9 +113,10 @@ st.dataframe(notes_table[['Player','Type','Date','Coach','Note']],hide_index=Tru
 
 # Get video rows for this player, sorted by most recent
 def display_video():
+    player_name = player_options[player_select]  # Get the player name from player_options
     player_videos = video[video['player_id'] == player_select]
     for index, row in player_videos.iterrows():
-        st.write(f"{player_select} - {row['type']} - {row['pitch_type']}")
+        st.write(f"{player_name} - {row['speed']} - {row['pitch_type']}")
         st.video(row['url'])
 
 st.subheader("Video:")
