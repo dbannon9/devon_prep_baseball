a1efb40f7371aa049a0940d4d33fa7d1e95fc4a2
Dan Bannon <danbannon9@gmail.com>
2025-01-23 09:46:22 -0500

    add arm tracking to home page

--- app/devon-prep-coaches-app.py
+++ app/devon-prep-coaches-app.py
@@ -15,6 +15,7 @@ roster = st.Page("pages/roster-page.py",title="Roster",icon=":material/tsunami:"
 note_input = st.Page("pages/note-input-page.py",title="Input Notes",icon=":material/edit_note:")
 player_page = st.Page("pages/player-page.py",title="Player Summary",icon=":material/bar_chart:")
 team_notes_page = st.Page("pages/team-notes-page.py",title="Team Notes",icon=":material/group:")
+arm_tracking_page = st.Page("pages/arm-tracking-page.py",title="Arm Tracking",icon=":material/flag:")
 # coach_page = st.Page("pages/coaches-page.py",title="Coach Summary",icon=":material/sports:")
 calendar_page = st.Page("pages/calendar-page.py",title="Schedule",icon=":material/calendar_month:")
 view_practice_plans_page = st.Page("pages/view-practice-plans-page.py",title="View Practice Plans",icon=":material/sports:")
@@ -45,6 +46,7 @@ if authenticate():
         note_input,
         player_page,
         team_notes_page,
+        arm_tracking_page,
         calendar_page,
         view_practice_plans_page,
         practice_planning_page,
