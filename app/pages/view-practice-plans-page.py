5339020130572e96ee726b81e5760cd9551d7bec
Dan Bannon <danbannon9@gmail.com>
2025-01-26 11:04:32 -0500

    fixing no practice problem

--- app/pages/view-practice-plans-page.py
+++ app/pages/view-practice-plans-page.py
@@ -40,79 +40,67 @@ practice_plans.set_index('id',inplace=True)
 st.title("View Practice Plans")
 pdate = st.date_input("Select Practice Date", value=date.today())
 pdate = pdate.strftime('%Y-%m-%d')
-this_practice = practice_plans[practice_plans['date']==pdate]
+this_practice = practice_plans[practice_plans['date'] == pdate]
+
+# Check if this_practice is empty
 if this_practice.empty:
     st.write("No practice plan found for this date.")
-
-# assign practice text names
-
-print_1 = f"{str(this_practice.iloc[0]['event_1_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_1_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_1_name']}" if this_practice.iloc[0]['event_1_name'] != "" else ""
-print_2 = f"{str(this_practice.iloc[0]['event_2_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_2_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_2_name']}" if this_practice.iloc[0]['event_2_name'] != "" else ""
-print_3 = f"{str(this_practice.iloc[0]['event_3_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_3_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_3_name']}" if this_practice.iloc[0]['event_3_name'] != "" else ""
-print_4 = f"{str(this_practice.iloc[0]['event_4_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_4_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_4_name']}" if this_practice.iloc[0]['event_4_name'] != "" else ""
-
-print_note_1 = f"{this_practice.iloc[0]['event_1_notes']}"
-print_note_2 = f"{this_practice.iloc[0]['event_2_notes']}"
-print_note_3 = f"{this_practice.iloc[0]['event_3_notes']}"
-print_note_4 = f"{this_practice.iloc[0]['event_4_notes']}"
-
-# add in PDF button
-def create_download_link(val, filename):
-    b64 = base64.b64encode(val)  # val looks like b'...'
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
-
-export_as_pdf = st.button("Export Practice Plans")
-
-if export_as_pdf:
-    pdf = FPDF()
-    pdf.add_page()
-    pdf.set_font('Helvetica', 'B', 24)
-    pdf.cell(40, 10, f"Practice Plans: {pdate}", ln=2)
-    pdf.set_font('Helvetica', 'I', 16)
-    pdf.cell(40, 10, f"{str(print_1)}", ln=2)
-    pdf.set_font('Helvetica', '', 12)
-    pdf.cell(40,10,f"{str(print_note_1)}", ln=2)
-    if this_practice.iloc[0]['event_2_name'] == '':
-        print("")
-    else:
-        pdf.set_font('Helvetica', 'I', 16)
-        pdf.cell(40, 10, f"{str(print_2)}", ln=2)
-        pdf.set_font('Helvetica', '', 12)
-        pdf.cell(40,10,f"{str(print_note_2)}", ln=2)
-    if this_practice.iloc[0]['event_3_name'] == '':
-        print("")
-    else:
-        pdf.set_font('Helvetica', 'I', 16)
-        pdf.cell(40, 10, f"{str(print_3)}", ln=2)
-        pdf.set_font('Helvetica', '', 12)
-        pdf.cell(40,10,f"{str(print_note_3)}", ln=2)
-    if this_practice.iloc[0]['event_4_name'] == '':
-        print("")
-    else:
-        pdf.set_font('Helvetica', 'I', 16)
-        pdf.cell(40, 10, f"{str(print_4)}", ln=2)
-        pdf.set_font('Helvetica', '', 12)
-        pdf.cell(40,10,f"{str(print_note_4)}", ln=2)
-    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
-    st.markdown(html, unsafe_allow_html=True)
-
+    st.stop()
 else:
+    # Assign practice text names
+    print_1 = f"{str(this_practice.iloc[0]['event_1_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_1_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_1_name']}" if this_practice.iloc[0]['event_1_name'] != "" else ""
+    print_2 = f"{str(this_practice.iloc[0]['event_2_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_2_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_2_name']}" if this_practice.iloc[0]['event_2_name'] != "" else ""
+    print_3 = f"{str(this_practice.iloc[0]['event_3_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_3_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_3_name']}" if this_practice.iloc[0]['event_3_name'] != "" else ""
+    print_4 = f"{str(this_practice.iloc[0]['event_4_start_time'])[:5].lstrip('0')} - {str(this_practice.iloc[0]['event_4_end_time'])[:5].lstrip('0')} - {this_practice.iloc[0]['event_4_name']}" if this_practice.iloc[0]['event_4_name'] != "" else ""
+
+    print_note_1 = f"{this_practice.iloc[0]['event_1_notes']}"
+    print_note_2 = f"{this_practice.iloc[0]['event_2_notes']}"
+    print_note_3 = f"{this_practice.iloc[0]['event_3_notes']}"
+    print_note_4 = f"{this_practice.iloc[0]['event_4_notes']}"
+
+    # Add in PDF button
+    def create_download_link(val, filename):
+        b64 = base64.b64encode(val)  # val looks like b'...'
+        return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="{filename}.pdf">Download file</a>'
+
+    export_as_pdf = st.button("Export Practice Plans")
+
+    if export_as_pdf:
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font('Helvetica', 'B', 24)
+        pdf.cell(40, 10, f"Practice Plans: {pdate}", ln=2)
+        pdf.set_font('Helvetica', 'I', 16)
+        pdf.cell(40, 10, f"{str(print_1)}", ln=2)
+        pdf.set_font('Helvetica', '', 12)
+        pdf.cell(40, 10, f"{str(print_note_1)}", ln=2)
+        if this_practice.iloc[0]['event_2_name'] != '':
+            pdf.set_font('Helvetica', 'I', 16)
+            pdf.cell(40, 10, f"{str(print_2)}", ln=2)
+            pdf.set_font('Helvetica', '', 12)
+            pdf.cell(40, 10, f"{str(print_note_2)}", ln=2)
+        if this_practice.iloc[0]['event_3_name'] != '':
+            pdf.set_font('Helvetica', 'I', 16)
+            pdf.cell(40, 10, f"{str(print_3)}", ln=2)
+            pdf.set_font('Helvetica', '', 12)
+            pdf.cell(40, 10, f"{str(print_note_3)}", ln=2)
+        if this_practice.iloc[0]['event_4_name'] != '':
+            pdf.set_font('Helvetica', 'I', 16)
+            pdf.cell(40, 10, f"{str(print_4)}", ln=2)
+            pdf.set_font('Helvetica', '', 12)
+            pdf.cell(40, 10, f"{str(print_note_4)}", ln=2)
+        html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
+        st.markdown(html, unsafe_allow_html=True)
+
     # Extract the event name for the row
-    # event_1_name = this_practice.iloc[0]['event_1_name']
     st.subheader(print_1)
     st.write(print_note_1)
-    if this_practice.iloc[0]['event_2_name'] == "":
-        print("")
-    else:
+    if this_practice.iloc[0]['event_2_name'] != "":
         st.subheader(print_2)
         st.write(print_note_2)
-        if this_practice.iloc[0]['event_3_name'] == "":
-            print("")
-        else:
-            st.subheader(print_3)
-            st.write(print_note_3)
-            if this_practice.iloc[0]['event_4_name'] == "":
-                print("")
-            else:
-                st.subheader(print_4)
-                st.write(print_note_4)
+    if this_practice.iloc[0]['event_3_name'] != "":
+        st.subheader(print_3)
+        st.write(print_note_3)
+    if this_practice.iloc[0]['event_4_name'] != "":
+        st.subheader(print_4)
+        st.write(print_note_4)
