8ceeb94e259f28fc7bcb8cb8d722f8b50d7c23fa
Dan Bannon <danbannon9@gmail.com>
2025-02-06 16:27:26 -0500

    na error mitigation

--- app/pages/player-page.py
+++ app/pages/player-page.py
@@ -137,7 +137,7 @@ if len(player_raphit) < 1:
 else:
     ev_max = max(player_raphit['ExitVelocity'])
     ev_avg = pd.to_numeric(player_raphit['ExitVelocity'],errors='coerce').mean()
-    ev_90 = np.percentile(player_raphit['ExitVelocity'], 90)
+    ev_90 = np.percentile(pd.to_numeric(player_raphit['ExitVelocity'],errors='coerce').dropna(), 90)
     st.write(f"Max EV: {ev_max}; 90th pct EV: {ev_90}; Average EV: {ev_avg}")
 
 # Get video rows for this player, sorted by most recent
