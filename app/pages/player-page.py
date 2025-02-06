3e510b1d172e1a16302e6952a98789de949fb916
Dan Bannon <danbannon9@gmail.com>
2025-02-06 16:12:14 -0500

    filtering rapsodo data on player page

--- app/pages/player-page.py
+++ app/pages/player-page.py
@@ -47,6 +47,10 @@ rapsodo_pitching.set_index('id',inplace=True)
 
 #%% Data Adjustments
 
+# define average()
+def average(series):
+    return sum(series) / len(series) if series else 0
+
 # assign class levels to index of years
 classdict = {
         0: "Middle",
@@ -124,8 +128,15 @@ st.dataframe(notes_table[['Player','Type','Date','Coach','Note']],hide_index=Tru
 # merge player_id onto rapsodo data
 raphit = rapsodo_hitting.merge(players,left_on='Player ID', right_on='rapsodo_id', how='left')
 player_raphit = raphit[raphit['player_id']==player_select].replace("-",np.nan)
-max_ev = max(player_raphit['ExitVelocity'])
-st.write(max_ev)
+
+# generate stats
+if len(player_raphit) < 1:
+    st.write('No Rapsodo Hitting Statistics')
+else:
+    ev_max = max(player_raphit['ExitVelocity'])
+    ev_avg = average(player_raphit['ExitVelocity'])
+    ev_90 = np.percentile(player_raphit['ExitVelocity'], 90)
+    st.write(f"Max EV: {ev_max}; 90th pct EV: {ev_90}; Average EV: {ev_avg}")
 
 # Get video rows for this player, sorted by most recent
 def display_video():
