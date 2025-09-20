db412b322402d572434a44edf496838a57d6fa5e
Dan Bannon <danbannon9@gmail.com>
2025-09-20 17:41:02 -0400

    updating supabase connection across apps

--- app/pages/roster-page2.py
+++ app/pages/roster-page2.py
@@ -7,28 +7,27 @@ from datetime import date, time
 import math
 from decimal import Decimal
 import os
-from supabase import Client, create_client
+from st_supabase_connection import SupabaseConnection
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
 #%% Connect to Supabase
 
 # Use st.secrets to load the URL and key from secrets.toml
-supabase_url = st.secrets["supabase"]["SUPABASE_URL"]
-supabase_key = st.secrets["supabase"]["SUPABASE_KEY"]
+# supabase_url = st.secrets["supabase"]["SUPABASE_URL"]
+# supabase_key = st.secrets["supabase"]["SUPABASE_KEY"]
 
-# get secrets from toml
-supabase: Client = create_client(supabase_url, supabase_key)
+db = st.connection("supabase",type=SupabaseConnection)
 
-# Create the connection object using SupabaseConnection
-db = create_client(supabase_url, supabase_key)
+# # Create the connection object using SupabaseConnection
+# db = create_client(supabase_url, supabase_key)
 
 #%% Data Retrieval
 
 # Function to fetch data from any table
 def fetch_table_data(table_name):
     # Execute the SQL query
-    df = supabase.table(f"{table_name}").select("*").execute().data
+    df = db.table(f"{table_name}").select("*").execute().data
     
     # Convert the fetched data into a pandas DataFrame
     return pd.DataFrame(df)
