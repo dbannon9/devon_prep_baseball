5f57461fa25b3a346f438bf0ef279746696fa980
Dan Bannon <danbannon9@gmail.com>
2025-09-20 14:30:14 -0400

    start over

--- app/pages/roster-page.py
+++ app/pages/roster-page.py
@@ -1,54 +1,54 @@
 #%% Imports
 
-import pandas as pd
-import numpy as np
 import streamlit as st
-from datetime import date, time
-import math
-from decimal import Decimal
-import os
-from supabase import create_client, Client
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
+# import pandas as pd
+# import numpy as np
+# from datetime import date, time
+# import math
+# from decimal import Decimal
+# import os
+# from supabase import create_client, Client
+# import matplotlib.pyplot as plt
+# import matplotlib.colors as mcolors
 
-#%% Connect to Supabase
+# #%% Connect to Supabase
 
-# Use st.secrets to load the URL and key from secrets.toml
-supabase_url = st.secrets["supabase"]["SUPABASE_URL"]
-supabase_key = st.secrets["supabase"]["SUPABASE_KEY"]
+# # Use st.secrets to load the URL and key from secrets.toml
+# supabase_url = st.secrets["supabase"]["SUPABASE_URL"]
+# supabase_key = st.secrets["supabase"]["SUPABASE_KEY"]
 
-# get secrets from toml
-supabase: Client = create_client(supabase_url, supabase_key)
+# # get secrets from toml
+# supabase: Client = create_client(supabase_url, supabase_key)
 
-# Create the connection object using SupabaseConnection
-db = create_client(supabase_url, supabase_key)
+# # Create the connection object using SupabaseConnection
+# db = create_client(supabase_url, supabase_key)
 
-#%% Data Retrieval
+# #%% Data Retrieval
 
-# Function to fetch data from any table
-def fetch_table_data(table_name):
-    # Execute the SQL query
-    df = supabase.table(f"{table_name}").select("*").execute().data
+# # Function to fetch data from any table
+# def fetch_table_data(table_name):
+#     # Execute the SQL query
+#     df = supabase.table(f"{table_name}").select("*").execute().data
     
-    # Convert the fetched data into a pandas DataFrame
-    return pd.DataFrame(df)
+#     # Convert the fetched data into a pandas DataFrame
+#     return pd.DataFrame(df)
 
-# Fetch data from all tables, then align id to supabase index
-players = fetch_table_data('players')
-players.set_index('id',inplace=True)
-coaches = fetch_table_data('coaches')
-coaches.set_index('id',inplace=True)
-notes = fetch_table_data('notes')
-notes.set_index('id',inplace=True)
-rapsodo_hitting = fetch_table_data('rapsodo_hitting')
-rapsodo_hitting.set_index('id',inplace=True)
-rapsodo_pitching = fetch_table_data('rapsodo_pitching')
-rapsodo_pitching.set_index('id',inplace=True)
+# # Fetch data from all tables, then align id to supabase index
+# players = fetch_table_data('players')
+# players.set_index('id',inplace=True)
+# coaches = fetch_table_data('coaches')
+# coaches.set_index('id',inplace=True)
+# notes = fetch_table_data('notes')
+# notes.set_index('id',inplace=True)
+# rapsodo_hitting = fetch_table_data('rapsodo_hitting')
+# rapsodo_hitting.set_index('id',inplace=True)
+# rapsodo_pitching = fetch_table_data('rapsodo_pitching')
+# rapsodo_pitching.set_index('id',inplace=True)
 
 #%% DIAGNOSTIC
 
-st.write("Columns in players:", players.columns.tolist())
-st.write(players.head())
+st.write("earth to streamlit")
+# st.write(players.head())
 
 
 
