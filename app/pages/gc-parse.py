116829e3b4b3d1fc98b360ac4c07fd608ce3e66a
Dan Bannon <danbannon9@gmail.com>
2026-05-10 18:22:38 -0400

    parse text

--- app/pages/gc-parse.py
+++ app/pages/gc-parse.py
@@ -125,6 +125,6 @@ if not txtdata.empty:
     txtdata['is_pitch_sequence'] = txtdata['text'].str.findall(pattern).str.len() > 0
     txtdata['is_score_change'] = txtdata['text'].str.contains(dp_team_abbrev)
     cols = ['is_pa_result','is_out_change','is_pitch_sequence','is_score_change']
-    txtdata['is_outcome_string'] = ~txtdata['cols'].any(axis=1)
+    txtdata['is_outcome_string'] = ~txtdata[cols].any(axis=1)
 
 txtdata
